969e17a34415a0e8adc5b11fe28a22ffece16d8b
AM111D <Dimasssnike@icloud.com>
2023-04-13 23:41:59 -0700

    add_filter

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Filter extends Component {
+
+    state = { 
+        filter: '',
+     };
+    }
+    render() {
+        return (
+            <>
+            <input type='text' name='search' id='' />
+            </>
+        );
+    }
+
+export default Filter;
